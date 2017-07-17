73cc753c23e388c35de9412f439b0292efac8531
DJQ666 <1689385797@qq.com>
2017-07-17 11:30:09 +0800

    登录

--- app/src/main/java/com/hitsoft/dab/MainActivity.java
+++ app/src/main/java/com/hitsoft/dab/MainActivity.java
@@ -2,6 +2,7 @@ package com.hitsoft.dab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       TabHost tabHost=(TabHost) findViewById(R.id.tabhost);
+        tabHost.setup();
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("特价").setContent(R.id.View1));
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("推荐").setContent(R.id.View2));
     }
 }
