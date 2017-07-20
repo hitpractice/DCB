1820d51694b69ccda03c4952e7da2275dcbea489
liuyuchen9 <711023LYClyc>
2017-07-20 09:40:12 +0800

    liuyuchen--zhuce,denglu

---
+++ app/src/main/java/com/hitsoft/dab/GetIdFromString.java
@@ -0,0 +1,22 @@
+package com.hitsoft.dab;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GetIdFromString {
+    public int getID(String string) {
+        String id = "";
+        char[] c = string.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] == '[') {
+                for (int j = i + 1; j < c.length; j++) {
+                    if (c[j] == ']')
+                        break;
+                    else
+                        id += c[j];
+                }
+            }
+        }
+        return Integer.parseInt(id);
+    }
+}
